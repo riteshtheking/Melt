0a3897c11cc220585997a1e6de29b0aba781034a
Ritesh Kushwaha <admin@craze.gq>
2018-06-24 06:22:13 +0530

    Create melt.js

---
+++ dist/js/melt.js
@@ -0,0 +1,34 @@
+        function zoomIn() {
+         var elluminati = document.getElementsByTagName("body")[0];
+          elluminati.style.zoom = "131%";
+        }
+
+
+        function blueFilter() {
+         var ellumint = document.getElementsByTagName("body")[0];
+          ellumint.style.filter = "sepia()";
+        }
+
+
+        function conTrast() {
+         var ellumnt = document.getElementsByTagName("body")[0];
+          ellumnt.style.filter = "contrast(4)";
+        }
+
+
+        function defaultTrew() {
+         var ellmnt = document.getElementsByTagName("body")[0];
+          ellmnt.style.filter = "none";
+          ellmnt.style.zoom = "100%";
+        }
+
+
+        function grayScale() {
+         var elmnit = document.getElementsByTagName("body")[0];
+          elmnit.style.filter = "grayscale()";
+        }
+
+        function blurIn() {
+         var elmunt = document.getElementsByTagName("body")[0];
+          elmunt.style.filter = "blur(1px)";
+        }        
